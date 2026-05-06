f228dd53c44979368396c5b3edeff8f3a777ae27
Gleyce Pereira <142240220+GleycePereira@users.noreply.github.com>
2026-05-06 18:26:14 -0300

    update

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { logoGoogle} from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab4',
@@ -15,12 +17,18 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput } from '@ionic/an
     IonToolbar, 
     CommonModule, 
     FormsModule,
-    IonInput
+    IonInput,
+    IonButton,
+    IonIcon
   ]
 })
 export class Tab4Page implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({
+      'logoGoogle': logoGoogle
+    });
+   }
 
   ngOnInit() {
   }
