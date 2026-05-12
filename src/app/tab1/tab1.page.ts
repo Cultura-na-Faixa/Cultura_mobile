8d372c7621d160581a5cb92ebbbb1d510e9456c5
Gleyce Pereira <142240220+GleycePereira@users.noreply.github.com>
2026-05-11 21:31:57 -0300

    update tab1
    update tab1 modal

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonButton, IonModal, IonButtons } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tab1',
@@ -19,14 +20,42 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
     IonCardTitle,
     IonButton,
     IonModal,
-    IonButtons
+    IonButtons,
+    CommonModule
   ],
 })
 export class Tab1Page {
+
   isModalOpen = false;
 
-  setOpen(isOpen: boolean) {
-    this.isModalOpen = isOpen;
+  eventoSelecionado: any;
+
+  eventos = [
+    {
+      titulo: 'Carlinhos do Teclado',
+      local: 'Sesc Pompeia',
+      data: '20/12 13h às 15h',
+      imagem: 'https://files.pressmanager.net/clientes/625b13d2e0f9dc14e52ac4deac083e71/imagens/2025/02/18/82817132bcc406c20824c21a3c2e52e2.jpg',
+      descricao: 'O show contará com grandes sucessos do artista.'
+    },
+
+    {
+      titulo: 'Ricardo & Morenin',
+      local: 'Vale do Anhangabaú',
+      data: '18/09 18h às 20h',
+      imagem: 'https://p2.trrsf.com.br/image/fget/cf/478/481/smart/images.terra.com/2025/07/08/302523534-8-dupla-sertaneja-felipe-e-matheus-viralizou-em-2024-ao-fazer-show-sem-plateia.jpg',
+      descricao: 'Show sertanejo com participação especial.'
+    }
+  ];
+
+  abrirModal(evento: any) {
+    this.eventoSelecionado = evento;
+    this.isModalOpen = true;
   }
+
+  fecharModal() {
+    this.isModalOpen = false;
+  }
+
   constructor() { }
 }
