20ab561cf5331434058bd2f2af48bef22193e1e3
Gleyce Pereira <142240220+GleycePereira@users.noreply.github.com>
2026-05-18 21:19:53 -0300

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://euwwnmtpqnwqeoodvfpd.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1d3dubXRwcW53cWVvb2R2ZnBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NjQ0NDIsImV4cCI6MjA5NDM0MDQ0Mn0.J-jCooHig09KKugagFRNP2GKqJMOUjLSB9cFmIT9jpM'
 };
